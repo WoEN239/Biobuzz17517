d077bab74da1326118bd4359ecf19ab72e0a625e
tikho <tikhonsmovzh@gmail.com>
2026-08-06 14:30:26 +0300

    test op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.kt
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp
+class OpMode: LinearOpMode() {
+    override fun runOpMode() {
+        waitForStart()
+        resetRuntime()
+
+        while (opModeIsActive()){
+
+        }
+    }
+}
